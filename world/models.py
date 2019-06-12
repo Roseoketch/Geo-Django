180f72ac4e664a9b11670766c2f6e8c7185992ba
Roseoketch <oketchrose@gmail.com>
2019-06-12 12:09:36 +0300

    initial commit

---
+++ world/models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class shop(models = Model):
+    name = models.CharField(max_length=30)
+    location = models.CharField(max_length=30)
+
+    # def__str__(self):
+    #     return self.name
