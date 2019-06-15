a7d0c19112b300f1853c191c5edbc3e67a65e227
rose.oketch <@oketchrose@gmail.com>
2019-06-15 11:00:40 +0300

    adding tables to the models

--- world/models.py
+++ world/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 # Create your models here.
 
-class shop(models = Model):
+class shop(models.Model):
     name = models.CharField(max_length=30)
     location = models.CharField(max_length=30)
 
